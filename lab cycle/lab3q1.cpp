4b82ecc96fa87f20974e0beca47fd49c8c08c676
HiraMohammedK <98226271+HiraMohammedK@users.noreply.github.com>
2022-04-04 01:19:54 +0530

    Add files via upload

---
+++ lab cycle/lab3q1.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+class Shape 
+{
+   protected:
+    float dimension;
+
+   public:
+    void getDimension() 
+    {
+        cin >> dimension;
+    }
+
+    virtual float calculateArea() = 0;
+};
+
+class Square : public Shape 
+{
+   protected:
+    float areaSquare;
+
+   public:
+    float calculateArea() 
+    {
+        areaSquare=dimension * dimension;
+        cout<<"Area of square = " << areaSquare <<endl;
+    }
+};
+
+class Circle : public Shape 
+{
+   protected:
+    float areaCircle;
+   public:
+    float calculateArea() 
+    {
+        areaCircle = 3.14 * dimension * dimension;
+        cout<<"Area of circle = " << areaCircle <<endl;
+    }
+};
+
+int main() 
+
+{
+     int option,choice,ch;
+    cout<<"\tWELCOME\t" <<endl;
+    cout<<"Do you want to continue?" <<endl;
+    cout<<"If yes enter 1 if no enter 0" <<endl;
+    cin>>option;
+    if(option==0)
+    {
+        cout<<"THANK YOU" <<endl;
+        return 0;
+    }
+    
+    
+    Shape *shapeptr;
+   
+    do
+    {
+        cout << "Select an option : \n 1:Area of Circle\n 2:Area of Square\n 3:Quit\n" <<endl;
+        cin >> choice;
+        switch (choice)
+        {
+            case 1 :
+            {
+                shapeptr = new Circle;
+                cout << "\nEnter radius of the circle: ";
+                shapeptr ->getDimension();
+                shapeptr ->calculateArea();
+                break;
+            }
+            case 2 :
+            {
+                shapeptr = new Square;
+                cout << "Enter the length of the square: ";
+                shapeptr ->getDimension();
+                shapeptr ->calculateArea();
+                break;
+            }
+            case 3 :
+            {
+                break;
+            }
+            default :
+            cout << "Try Again" <<endl;
+            break;
+        }
+        cout<<"Do you want to continue?" <<endl;
+        cout<<"If yes enter 1 if no enter 0" <<endl;
+        cin>>ch;
+    }while(ch);
+    
+    if(ch==0)
+    {
+        cout<<"THANK YOU" <<endl;
+        return 0;
+
+    
+    }
+    
+
+}
